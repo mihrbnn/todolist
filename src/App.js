9419a8119a4952062f3c6643f3ca19e76b4f7434
mihrbnn <mihkilickan@gmail.com>
2021-12-14 13:27:39 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+
+import "./App.css";
+import { useState } from "react";
+import ListItems from "./components/ListItems";
 
 function App() {
+  const [list, setList] = useState([])
+  function eventSubmit(e) {
+    e.preventDefault()
+    if (e.target[0].value) {
+      setList([...list, { value: e.target[0].value, isDone: false }])  
+      e.target[0].value=""
+    }   
+  }
+
+  function taskCompleted(item){
+    const newList = list.map(value => {
+      if (item === value) {
+      value.isDone=!value.isDone
+      console.log(value)
+      }
+    return value
+    })
+    setList(newList)
+
+  }
+
+  function deleteItem(value) {
+    const newList = list.filter(
+      i=>i.value !== value.value
+    );
+    setList(newList)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container col-sm-6 mt-5">
+      <form onSubmit={(e)=>eventSubmit(e)}>
+        <div className="mb-3">
+          <input
+            type="text"
+            className="form-control"
+            id="exampleInputEmail1"
+            placeholder="bir görev giriniz"
+          />
+         
+        </div>
+        <button type="submit" className="btn btn-primary mb-3 ">
+          Ekle
+        </button>
+      </form>
+      <div>
+          <ul className="list-group">
+            {
+            list.map((item, index) => <ListItems key={index} item={item} deleteItem={() => deleteItem(item)} updateItem={()=>taskCompleted(item)}/>)
+            }
+          </ul> 
+      </div>
     </div>
   );
 }
---
+++ src/components/ListItems.jsx
@@ -0,0 +1,12 @@
+
+function ListItems(props) {
+
+    return (
+        <li className={`d-flex flex-row list-group-item fw-bol justify-content-between align-items-center text-white ${props.item.isDone ? "bg-success text-decoration-line-through" : "bg-danger"}`} >
+            <span className="w-100" onClick={() => props.updateItem()}>{props.item.value}</span>
+        <button type="button" className="btn text-white text-decoration-none" onClick={()=>props.deleteItem()}>x</button>
+        </li>
+    )  
+}
+
+export default ListItems
